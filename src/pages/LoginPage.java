73bd6c98d92a7d2ed1e13c44541358b466a84717
Rameshk1998 <rameshkashapogu1998@gmail.com>
2023-02-16 13:55:15 +0530

    Added/Updated/Deleted file

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,6 +4,9 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 
+		System.out.println("Hello LP");
+		String us = "admin";
+		String pw = "admin123";
 		System.out.println();
 
 	}
--- src/tests/LoginPageTest.java
+++ src/tests/LoginPageTest.java
@@ -2,4 +2,7 @@ package tests;
 
 public class LoginPageTest {
 
+	public void test() {
+		System.out.println("Test 1 LP");
+	}
 }
