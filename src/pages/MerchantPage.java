30e949c578710398081eb746ff5f71425d195114
Rameshk1998 <rameshkashapogu1998@gmail.com>
2023-02-19 12:12:56 +0530

    Merchant changes added

--- src/pages/MerchantPage.java
+++ src/pages/MerchantPage.java
@@ -3,8 +3,9 @@ package pages;
 public class MerchantPage {
 
 	public static void main(String[] args) {
-
-		System.out.println("Merchant dev 2");
+		
+		System.out.println("Merchant dev 1");
+		int merchantId;
 	}
 
 }
