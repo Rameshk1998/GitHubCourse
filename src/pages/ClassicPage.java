c357088536c232567361c227e65b0ffe1bae871a
Rameshk1998 <125430361+Rameshk1998@users.noreply.github.com>
2023-02-19 12:57:33 +0530

    Create ClassicPage.java

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class ClassicPage {
+
+	public void testpage() {
+		System.out.println("Classic div");
+		int a=100;
+		int b=10;
+		int div =a/b;;
+		System.out.println(div);
+	}
+
+}
