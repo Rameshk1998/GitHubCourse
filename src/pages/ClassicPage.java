4df47160f3c34ca5261c504fba2025b539df93c8
Rameshk1998 <rameshkashapogu1998@gmail.com>
2023-02-19 12:58:02 +0530

    Classic Page added

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class ClassicPage {
+
+	public void testpage() {
+		System.out.println("Classic");
+		int i=10;
+		int j=20;
+		int sum =i+j;
+		System.out.println(sum);
+	}
+
+}
